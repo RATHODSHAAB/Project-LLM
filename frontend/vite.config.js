7dd4d4c4541d69d2b727b718d318fa552280fd2a
deeprathoree <deependrathore9799@gmail.com>
2025-12-23 18:12:01 +0530

    error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://project-llm.onrender.com')
+  }
 })
